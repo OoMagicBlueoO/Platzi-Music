ac6552b66db9c094689de715f5395e3909143927
Luis Hernandez <darkshield@live.com.mx>
2018-01-28 21:47:05 -0600

    Add component Artist

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,12 @@
     img(src='./assets/logo.png')
     h1 Platzi-Music
     ul
-      li(v-for='artist in artists') {{ artist.name}}
+      artist(v-for='artist in artists' v-bind:artist="artist" v-bind:key="artist.mbid") {{ artist.name}}
 
 
 </template>
 <script>
+import Artist from './components/Artist.vue'
 import getArtists from './api';
 export default {
   name: 'app',
@@ -15,6 +16,9 @@ export default {
     return {
       artists: []
     }
+  },
+  components:{
+    Artist
   },
   mounted: function(){
     const self = this
